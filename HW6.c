4b7540553bd34b41ec35b5f212684cfc6001c06e
Esteban <4801459559Ee>
2019-11-12 00:38:13 -0500

    finished hw 6

--- HW6.c
+++ HW6.c
@@ -8,28 +8,38 @@
 
 #define MAX_SIZE 100
 
-int main() {
+int main(void) {
 
     char string[MAX_SIZE] = "";
 
     int i;
-    int spaces;
-    int length = sizeof(string)/ sizeof();
+    int spaces = 0;
+    int length = sizeof(string)/ sizeof(string[0]);
 
 
     printf("Enter a string (up to 100 characters):");
-    gets_s(string, length);
+    gets_s(string, MAX_SIZE);
+
+
 
     for (i = 0; i < length; i++)
     {
         printf ("Char %c \n", string[i]);
+
+        if (string[i] == ' ') {
+            spaces++;
+        }
+
+        if (string[i] == '\0') {
+            break;
+        }
+
     }
 
+    printf("length %d \n", i);
+    printf("Spaces: %d \n", spaces);
     printf("String length %d \n", length);
 
     system("pause");
-
-
-
 
 }
